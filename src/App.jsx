9640524e026d3b8ea8a4bc328883c7663bd04856
Yodgorov Dilxushbek <dilxushbekyodgorov@gmail.com>
2025-01-13 21:00:43 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
+import Login from './pages/Login.jsx';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import ProtectedRoute from './components/ProtectedRoute';
